28c49d4a3d3844d0de27d3a22d0aabea425c064c
Kevin Thompson <kevinthompson.nmtcs@gmail.com>
2020-05-18 11:00:32 -0400

    Added decimal and delete button

--- js/script.js
+++ js/script.js
@@ -3,25 +3,28 @@
     let showDiv = document.querySelector('.show-div');
     showDiv.textContent = '';
     let buttons = document.querySelectorAll('.btn');
+    let decimalState = true;
 
     for(let i = 0; i < buttons.length; i++) {
         if (buttons[i].getAttribute('data-value') == 'clear') {
             buttons[i].addEventListener('click', clearScreen);
         } else if (buttons[i].getAttribute('data-value') == 'eval') {
             buttons[i].addEventListener('click', evaluateExpression);
+        } else if (buttons[i].getAttribute('data-value') == 'dot') {
+            buttons[i].addEventListener('click', addDecimal);
+        } else if (buttons[i].getAttribute('data-value') == 'del') {
+            buttons[i].addEventListener('click', deleteTerm);
         } else {
             buttons[i].addEventListener('click', buttonClick);
         }
         buttonList.push(buttons[i]);
     }
 
-
-
-
     function buttonClick(event) {
         const dataValue = this.getAttribute('data-value');
         if (dataValue == '+' || dataValue == '-' || dataValue == '*' || dataValue == '/') {
             showDiv.textContent += ` ${dataValue} `;
+            enableDecimal();
         } else {
             showDiv.textContent += dataValue;
         }
@@ -29,10 +32,12 @@
 
     function clearScreen(event) {
         showDiv.textContent = '';
+        enableDecimal();
     }
 
     function evaluateExpression(event) {
         let expression = showDiv.textContent.split(' ');
+        console.log(expression);
         expression = expression.map((item) => +item || item);
         let rpnExpression = convertToRPN(expression);
         let answer = evaluateRPN(rpnExpression);
@@ -41,6 +46,38 @@
         } else {
             showDiv.textContent = 'ERROR: Invalid Expression';
         }
+    }
+
+    function addDecimal(event) {
+        if (decimalState) {
+            showDiv.textContent += '.';
+            decimalState = false;
+            this.classList.toggle('btn-disabled');
+        }
+    }
+
+    function deleteTerm(event) {
+        let terms = showDiv.textContent.split(' ');
+        let lastTerm = terms[terms.length - 1];
+        if (lastTerm == '') {
+            terms.pop();
+            lastTerm = terms[terms.length - 1];
+        }
+        if(lastTerm[lastTerm.length - 1] == '.') {
+            enableDecimal();
+        }
+        lastTerm = lastTerm.substr(0, lastTerm.length - 1);
+        terms.pop();
+        if(lastTerm.length > 0) {
+            terms.push(lastTerm);
+        }
+        showDiv.textContent = terms.join(' ');
+        console.log(terms);
+    }
+
+    function enableDecimal() {
+        decimalState = true;
+        document.querySelector('[data-value="dot"]').classList.remove('btn-disabled');
     }
 
     function add(a, b) {
